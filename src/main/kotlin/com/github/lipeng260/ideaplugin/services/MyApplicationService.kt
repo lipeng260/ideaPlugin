52732940e34f9c5366c9c74509550eba1c50ec6d
GitHub Action <action@github.com>
2021-08-06 10:15:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/lipeng260/ideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lipeng260/ideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lipeng260.ideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lipeng260.ideaplugin.MyBundle
 
 class MyApplicationService {
 
